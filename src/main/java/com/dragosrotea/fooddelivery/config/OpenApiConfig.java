fc7f62ff85cb0a16738180e6216e3a6bf8d263c3
Dragos Rotea <dragosrotea2005@gmail.com>
2026-09-14 16:22:15 +0300

    feat: add OpenAPI documentation

---
+++ src/main/java/com/dragosrotea/fooddelivery/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.dragosrotea.fooddelivery.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    public static final String BEARER_AUTH = "bearerAuth";
+
+    @Bean
+    public OpenAPI foodDeliveryOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Food Delivery API")
+                        .version("1.0.0")
+                        .description("REST API for restaurants, menus, authentication and orders."))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
+                .components(new Components().addSecuritySchemes(
+                        BEARER_AUTH,
+                        new SecurityScheme()
+                                .name(BEARER_AUTH)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
