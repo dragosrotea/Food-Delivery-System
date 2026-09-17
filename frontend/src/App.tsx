e35bc66a36161a5058ebf1cd9ad385ae005072ee
Dragos Rotea <dragosrotea2005@gmail.com>
2026-09-17 12:29:04 +0300

    feat(frontend): route restaurant menus

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import AppLayout from "./layouts/AppLayout";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import RestaurantPage from "./pages/RestaurantPage";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/restaurants/:restaurantId" element={<RestaurantPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
